27dec6ea4c93d2b2a077e41ce5e9f95e8122b0dd
Rile-Cry <rileycriddle@yahoo.com>
2025-08-03 03:53:12 -0400

    Added Teleporter

--- scenes/environment/teleporter.gd
+++ scenes/environment/teleporter.gd
@@ -1,10 +1,22 @@
 extends Object_Interactable
 
-@export var exit:Node2D
+@export var exit : Object_Interactable
 
+var can_interact := true
 
-func overlap(object : Node2D) -> void:
-	print("Teleport " + str(object) + "to:" + str(exit))
-	
-	# TODO: Need a teleporter pad for the player, connected via an export
-# TODO linking teleporters and exporters
+func _ready() -> void:
+	super()
+	body_exited.connect(_on_body_exited)
+
+func overlap(player : Player) -> void:
+	if "can_interact" in exit:
+		if can_interact:
+			exit.can_interact = false
+			player.position = exit.position
+			player.move_target = player.position
+	else:
+		push_warning("The connected exit is not a teleporter.")
+
+func _on_body_exited(node: Node2D) -> void:
+	if node is Player and not can_interact:
+		can_interact = true
